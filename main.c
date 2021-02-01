75e154546b332604d2b9c961cb43e083f6c03f39
martinsspn <martinsspn@github.com>
2021-02-01 14:22:04 -0300

    finalizado

--- main.c
+++ main.c
@@ -13,39 +13,51 @@ int main(){
     if(pid<0){
         exit(-1);
     }else if(pid == 0){//processo filho1
-        printf("filho 1 (PID = %n) foi criado\n", getpid());
-        sleep(12);
+        printf("filho 1 (PID = %d) foi criado\n", getpid());
+        sleep(13);
         pid = fork();
         if(pid < 0){
             exit(-1);
         }else if(pid == 0){//processo neto1
             printf("neto 1 (PID = %d) foi criado\n", getpid());
+            sleep(12);
+            printf("neto 1 morreu (nascido no ano 26, viveu 12 anos)\n");
+            exit(3);
         }else{//processo filho1
-
+       		sleep(18);
+            printf("filho 1 morreu (nascido no ano 14, viveu 30 anos)\n");
+       		exit(1);
         }
     }else{//processo pai
         for(int i = 15; i <= 16; i++){
-            printf("Ano %d\n", i);
             sleep(1);
+            printf("Ano %d\n", i);
         }
         pid = fork();
         if(pid < 0){
             exit(-1);
         }else if(pid == 0){//processo filho2
             printf("filho 2 (PID = %d) foi criado\n", getpid());
-            sleep(14);
+            sleep(15);
             pid = fork();
             if(pid < 0){
                 exit(-1);
             }else if(pid == 0){//processo neto2
-                printf("neto 1 (PID = %d) foi criado\n", getpid());
+                printf("neto 2 (PID = %d) foi criado\n", getpid());
+            	sleep(18);
+	            printf("neto 2 morreu (nascido no ano 30, viveu 18 anos)\n");
+            	exit(4);
             }else{//processo filho2
-
+            	sleep(16);
+	            printf("filho 2 morreu (nascido no ano 16, viveu 30 anos)\n");
+       			exit(2);
             }
         }else{//processo pai
-            for(int i = 16; i <= 60; i++){
-                printf("Ano %d\n", i);
+            for(int i = 17; i <= 60; i++){
                 sleep(1);
+                printf("Ano %d\n", i);
+                if(i == 60)
+    	        printf("Pai morreu (nascido no ano 1, viveu 60 anos)\n");
             }
         }
     }
